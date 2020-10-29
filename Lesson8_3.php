a171fc395f2b3eefd647955b833beb58515901b2
松浦一樹 <kzkdrmlove@gmail.com>
2020-10-29 23:53:25 +0900

    complete9

---
+++ Lesson8_3.php
@@ -0,0 +1,8 @@
+[]で配列を作る
+
+<?php
+$a = ["sato", "suzuki", "takahashi" ];
+
+echo ($a[0])."\n";
+echo ($a[1])."\n";
+echo ($a[2])."\n";
---
+++ Lesson8_4.php
@@ -0,0 +1,10 @@
+
+<?php
+$a = ["sato", "suzuki", "takahashi" ];
+
+// データの中身を変えられる
+$a[1] = "tanaka";
+
+echo ($a[0])."\n";
+echo ($a[1])."\n";
+echo ($a[2])."\n";
